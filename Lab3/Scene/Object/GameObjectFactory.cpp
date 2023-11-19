d72e6fc9fc0e19a22bd3788a7a8fb89b7c2a95fd
levse <levse@POTATO>
2023-11-19 12:20:09 +0300

    applying textures to all objects

--- Lab3/Scene/Object/GameObjectFactory.cpp
+++ Lab3/Scene/Object/GameObjectFactory.cpp
@@ -15,6 +15,10 @@ void GameObjectFactory::init(std::string filename)
 	}
 	// загружаем весь исходный текст (до разделителя - нулевого символа)
 	//cout << "Pars " << filename << ":\n";
+
+	string typeMaterial;
+	string textureMaterial;
+
 	string jsonString;
 	getline(f, jsonString, static_cast<char>(0));
 	f.close();
@@ -42,42 +46,63 @@ void GameObjectFactory::init(std::string filename)
 				else {
 					if (itr->value.IsObject()) {
 						//cout << endl;
-						for (auto materialType = itr->value.MemberBegin(); materialType != itr->value.MemberEnd(); ++materialType) {
-							//cout << materialType->name.GetString() << " : ";
-							if (materialType->value.IsArray()) {
-								//cout << materialType->name.GetString() << " : ";
-								string nameTypeMaterial = materialType->name.GetString();
+						for (auto materialValue = itr->value.MemberBegin(); materialValue != itr->value.MemberEnd(); ++materialValue) {
+							//cout << materialValue->name.GetString() << " : ";
+							if (materialValue->value.IsString()) {
+								string nameTypeMaterial = materialValue->name.GetString();
+								if (nameTypeMaterial == "type") {
+									typeMaterial = materialValue->value.GetString();
+								}
+								if (nameTypeMaterial == "texture") {
+									textureMaterial = materialValue->value.GetString();
+								}
+							}
+							if (materialValue->value.IsArray()) {
+								//cout << materialValue->name.GetString() << " : ";
+								string nameParamsMaterial = materialValue->name.GetString();
 								//cout << nameTypeMaterial << " : ";
-
-								if (nameTypeMaterial != "shininess") {
-									color[0] = materialType->value[0].GetFloat();
-									color[1] = materialType->value[1].GetFloat();
-									color[2] = materialType->value[2].GetFloat();
-									color[3] = materialType->value[3].GetFloat();
-									if (nameTypeMaterial == "diffuse") {
+								if (nameParamsMaterial != "shininess") {
+									color[0] = materialValue->value[0].GetFloat();
+									color[1] = materialValue->value[1].GetFloat();
+									color[2] = materialValue->value[2].GetFloat();
+									color[3] = materialValue->value[3].GetFloat();
+									if (nameParamsMaterial == "diffuse") {
 										materialchik.Diffuse = color;
 									}
-									if (nameTypeMaterial == "ambient") {
+									if (nameParamsMaterial == "ambient") {
 										materialchik.Ambient = color;
 									}
-									if (nameTypeMaterial == "specular") {
+									if (nameParamsMaterial == "specular") {
 										materialchik.Specular = color;
 									}
-									if (nameTypeMaterial == "emission") {
+									if (nameParamsMaterial == "emission") {
 										materialchik.Emission = color;
 									}
 									color = {};
 								}
 							}
-							else {
-								materialchik.Shininess = materialType->value.GetFloat();
+							if(materialValue->value.IsFloat()) {
+								materialchik.Shininess = materialValue->value.GetFloat();
 								// указатель на базовый класс материала
-								shared_ptr<PhongMaterial> material = make_shared<PhongMaterial>();
-								material->load(materialchik);
-								materials.push_back(material);
+								if (typeMaterial == "PhongMaterialWithTexture") {
+									shared_ptr<Texture> TextureObj = make_shared<Texture>();
+									TextureObj->load(textureMaterial);
+
+									shared_ptr<PhongMaterialWithTexture> material = make_shared<PhongMaterialWithTexture>();
+									material->load(materialchik);
+									material->setTexture(TextureObj);
+									materials.push_back(material);
+								}
+								if (typeMaterial == "PhongMaterial") {
+									shared_ptr<PhongMaterial> material = make_shared<PhongMaterial>();
+									material->load(materialchik);
+									materials.push_back(material);
+								}
+								
 							}
+							//cout << endl;
 						}
-						//cout << endl;
+						//cout << endl<< endl;
 					}
 				}
 			}
